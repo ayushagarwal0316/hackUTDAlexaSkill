b879d3216eeed1e6183947d9e8b52582cb6a5321
Ayush Agarwal <ayush0316@gmail.com>
2019-02-24 00:22:37 -0600

    Add files via upload

---
+++ list.js
@@ -0,0 +1,43 @@
+module.exports = {
+  'DINING_OPEN_TIMES': {
+    'lemma coffee roasters': 'lemma coffee roasters is open from seven-thirty a m to six p m',
+    'the market at ecsw': 'the market at e c s west is open from eight a m to five p m',
+    'novel brew': 'novel brew is open from nine a m to four p m',
+    'food truck park': 'the food truck park is open from eleven a m to two p m',
+    'jason\'s deli': 'jason\'s deli is open from eleven a m to seven p m',
+    'the market at jsom': 'the market at j som is open from eight a m to nine p m',
+    'einstien bros bagels': 'einstien bros bagels is open from seven-thirty a m to four p m',
+    'ihop': 'i hop is open from eight a m to twelve a m',
+    'dining hall west': 'dining hall west is open from seven a m to nine a m',
+    'the market at rhw': 'the market at r h west is open from eleven a m to one a m',
+    'papa john\'s pizza': 'papa john\'s pizza is open from eleven a m to one a m',
+    'the market at ssa': 'the market at s s a is open from eight a m to five p m',
+    'ben and jerry\'s': 'ben and jerry\'s is open from eleven a m to eight p m',
+    'chic-fil-a': 'chic-fil-a is open from seven-thirty a m to eight p m',
+    'the market at su': 'the market at s u is open from nine a m to five p m',
+    'moes': 'moe\'s is open from eleven a m to eight p m',
+    'panda express': 'panda express is open from eleven a m to eight p m',
+    'the pub': 'the pub is open from eleven a m to ten p m',
+    'smash\'d': 'smash\'d is open from eleven a m to six p m',
+    'starbucks': 'starbucks is open from seven-thirty a m to ten p m',
+    'subway': 'subway is open from seven-thirty a m to eight p m',
+    'za\'tar': 'za\'tar is open from eleven a m to five p m',
+    'the market at vcb': 'the market at v c b is open from eight a m to three p m'
+  }
+
+  'PARKING_STRUCTURES': {
+    'ps4': 'REEEEEEEEEEEEEEEEEEE',
+    'ps1': 'real estate',
+    'ps3': 'save money, live better, walmart'
+  }
+
+  'DATES': {
+    'classes end': 'the last day of class is friday, may third',
+    'registraton open': 'class registration opens monday, october twenty second',
+    'second session': 'the second eight week session begins monday, march eleventh',
+    'first session': 'the first eight week session begins monday, january fourteenth',
+    'classes begin': 'the full term session begins monday, january fourteenth',
+    'final exams': 'final exams begin monday, may sixth and ends saturday, may eleventh',
+    'spring break': 'spring break begins monday, march eigth and ends sunday, march twenty fourth'
+  }
+}
